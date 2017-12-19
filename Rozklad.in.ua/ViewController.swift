c41922b5df7a47906968db3379aa31b4a9c2b1df
Artem Morozov <hoptoa@gmail.com>
2017-12-19 16:52:32 +0200

    Initial Commit

---
+++ Rozklad.in.ua/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rozklad.in.ua
+//
+//  Created by Artem Morozov on 12/19/17.
+//  Copyright © 2017 Rozklad Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
